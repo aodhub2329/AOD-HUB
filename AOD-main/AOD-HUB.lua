b4d9208704145fa5f7999e09e7feb35aab60e2a4
aodhub2329 <160031814+aodhub2329@users.noreply.github.com>
2024-02-15 11:21:26 +0700

    Update AOD-HUB.lua

--- AOD-main/AOD-HUB.lua
+++ AOD-main/AOD-HUB.lua
@@ -4401,6 +4401,67 @@ spawn(function()
   end
 end)
 end
+--------------------------------------------------------------------------------------------------
+--fast attack
+local plr = game.Players.LocalPlayer
+
+local CbFw = debug.getupvalues(require(plr.PlayerScripts.CombatFramework))
+local CbFw2 = CbFw[2]
+
+function GetCurrentBlade() 
+    local p13 = CbFw2.activeController
+    local ret = p13.blades[1]
+    if not ret then return end
+    while ret.Parent~=game.Players.LocalPlayer.Character do ret=ret.Parent end
+    return ret
+end
+function AttackNoCD() 
+    local AC = CbFw2.activeController
+    for i = 1, 1 do 
+        local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(
+            plr.Character,
+            {plr.Character.HumanoidRootPart},
+            60
+        )
+        local cac = {}
+        local hash = {}
+        for k, v in pairs(bladehit) do
+            if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
+                table.insert(cac, v.Parent.HumanoidRootPart)
+                hash[v.Parent] = true
+            end
+        end
+        bladehit = cac
+        if #bladehit > 0 then
+            local u8 = debug.getupvalue(AC.attack, 5)
+            local u9 = debug.getupvalue(AC.attack, 6)
+            local u7 = debug.getupvalue(AC.attack, 4)
+            local u10 = debug.getupvalue(AC.attack, 7)
+            local u12 = (u8 * 798405 + u7 * 727595) % u9
+            local u13 = u7 * 798405
+            (function()
+                u12 = (u12 * u9 + u13) % 1099511627776
+                u8 = math.floor(u12 / u9)
+                u7 = u12 - u8 * u9
+            end)()
+            u10 = u10 + 1
+            debug.setupvalue(AC.attack, 5, u8)
+            debug.setupvalue(AC.attack, 6, u9)
+            debug.setupvalue(AC.attack, 4, u7)
+            debug.setupvalue(AC.attack, 7, u10)
+            pcall(function()
+                for k, v in pairs(AC.animator.anims.basic) do
+                    v:Play()
+                end                  
+            end)
+            if plr.Character:FindFirstChildOfClass("Tool") and AC.blades and AC.blades[1] then 
+                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetCurrentBlade()))
+                game.ReplicatedStorage.Remotes.Validator:FireServer(math.floor(u12 / 1099511627776 * 16777215), u10)
+                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, i, "") 
+            end
+        end
+    end
+end
 --------------------------------------------------------------------------------------------------------------------------------------------
 --Setting
 local SettingFarm = Tabs.Setting:AddSection("Farming")
@@ -4418,52 +4479,44 @@ DropdownDelayAttack:OnChanged(function(Value)
 _G.FastAttackZedr_Mode = Value
 if _G.FastAttackZedr_Mode == "Fast Attack" then
     _G.Fast_Delay = 0.0005
+    local cac
+    print("fast")
     local SuperFastMode = true
+    if SuperFastMode then 
+        cac=task.wait
+    else
+        cac=wait
+    end
+    while cac() do 
+        if _G.FastAttackZedr_Mode == "NormalAttack" or "Super Fast Attack"then
+            break
+        end
+        AttackNoCD()
+        wait(0.05)
+    end
 elseif _G.FastAttackZedr_Mode == "Normal Attack" then
     _G.Fast_Delay = 0.01
     local SuperFastMode = true
 elseif _G.FastAttackZedr_Mode == "Super Fast Attack" then
     _G.Fast_Delay = 0.000005
+    local cac
+    print("super")
     local SuperFastMode = true
+    if SuperFastMode then 
+        cac=task.wait
+    else
+        cac=wait
+    end
+    while cac() do 
+        if _G.FastAttackZedr_Mode == "NormalAttack" or "Fast Attack"then
+            break
+        end
+        AttackNoCD()
+        wait(0.0005)
+    end
 end
 end)
 
-spawn(function()
-    local SuperFastMode = true
-    local cac
-    print("spawm")
-    while cac() do
-        if _G.FastAttackZedr_Mode == "Fast Attack" then
-            print("fast")
-            if SuperFastMode then 
-                cac=task.wait
-            else
-                cac=wait
-            end
-            while cac() do 
-                if _G.FastAttackZedr_Mode == "NormalAttack" or "Super Fast Attack"then
-                    break
-                end
-                AttackNoCD()
-                wait(0.05)
-            end
-        if _G.FastAttackZedr_Mode == "Super Fast Attack" then
-            print("Super")
-            local SuperFastMode = true
-            local cac
-            if SuperFastMode then 
-                cac=task.wait
-            else
-                cac=wait
-            end
-            while cac() do
-                AttackNoCD()
-                wait(0.0005)
-            end
-    end
-end
-end
-end)
 
     local ToggleBringMob = Tabs.Setting:AddToggle("ToggleBringMob", {Title = "Bring Mob",Description = "", Default = true })
     ToggleBringMob:OnChanged(function(Value)
